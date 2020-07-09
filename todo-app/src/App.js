c838159479bda08d643297b151089abc1c7f83df
ilmaticboy <jk3877@columbia.edu>
2020-07-09 21:27:40 +0900

    Feat: Completed building todoApp with React Hooks.
    The app supports adding, deleting, modifying options.

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -2,22 +2,38 @@ import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
+/*
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+const App = () => {
+  const [todos, setTodos] = useState(createBulkTodos);
+  const nextId = useRef(2501);
+*/
 
 const App = () => {
   const [todos, setTodos] = useState([
     {
       id: 1,
-      text: '리액트 기초 알아보기',
+      text: 'Studying React',
       checked: true,
     },
     {
       id: 2,
-      text: '컴포넌트 스타일링 해보기',
+      text: 'Studying Backend',
       checked: true,
     },
     {
       id: 3,
-      text: '일정 관리 앱 만들어 보기',
+      text: 'Commiting on GitHUb',
       checked: false,
     },
   ]);
@@ -48,11 +64,22 @@ const App = () => {
     [todos],
   );
 
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+
   return (
     <div>
       <TodoTemplate>
         <TodoInsert onInsert={onInsert} />
-        <TodoList todos={todos} onRemove={onRemove} />
+        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
       </TodoTemplate>
     </div>
   );
